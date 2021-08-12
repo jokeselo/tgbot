b47b894901b50958ac9007c027d8197add7032a3
fajisnm <87913488+fajisnm@users.noreply.github.com>
2021-08-12 16:11:24 +0530

    Create logo.py

---
+++ logo.py
@@ -0,0 +1,20 @@
+<p align="center">
+    <img
+        alt="White 'FN' text with black stroke in white background"
+        width="50%"
+        src="https://camo.githubusercontent.com/26f5e5ce1a2cb52a16ba82c6edb0015da071a327ecda3a349ebd783fa5d89723/68747470733a2f2f74656c656772612e70682f66696c652f3963333134663064323561613066366331353661322e6a7067"
+    />
+    <img
+        alt="Black 'FN' text with white stroke in black background"
+        width="50%"
+        src="https://camo.githubusercontent.com/b367f065008cc2e8fb92a720b3cadaf50ef6fbb0145fe5192ebdc7ec05cd425e/68747470733a2f2f74656c656772612e70682f66696c652f3633656161373232303032366461363437663065622e6a7067"
+    />
+</p>
+
+<p align="center">
+    <img 
+        alt="White 'FayasNoushad' text with black stroke in white background"
+        width="100%"
+        src="https://camo.githubusercontent.com/b0f5a142ca1e1a82cb4130a38ca1145fda99c529f039331085421ca4df3995f6/68747470733a2f2f74656c656772612e70682f66696c652f3862313831343634663762386135303265373939332e6a7067"
+    />
+</p>
