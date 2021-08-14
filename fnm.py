51de8d0bedfea1e7fed6ec640e98e261fa3d4767
fajisnm <87913488+fajisnm@users.noreply.github.com>
2021-08-14 11:23:22 +0530

    Update fnm.py

--- fnm.py
+++ fnm.py
@@ -1,10 +1,3 @@
-editable = await message.reply_text("Please wait ...")
+await message.reply_text("Please wait ...")
  
-            await editable.edit(
-                f"**Your File Stored in my Database!**\n\nHere is the Permanent Link of your file:  \n\nJust Click the link to get your file!",
-                parse_mode="Markdown",
-                reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("♻️Open Link♻️", url=https://t.me/mhdfajisn/5)],
-                     [InlineKeyboardButton("🔊Channel", url="https://t.me/Mo_Tech_Yt"),
-                      InlineKeyboardButton("👨‍💼Group", url="https://t.me/Mo_Tech_Group")]]
-                )
+         
