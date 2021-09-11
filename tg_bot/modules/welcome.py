a5071ab653511b739e3fc3fa030b3cc5525c0105
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-09-11 20:55:44 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -521,7 +521,7 @@ __help__ = """
 
 """.format(WELC_HELP_TXT)
 
-__mod_name__ = "Greetings"
+__mod_name__ = "Set Welcome Msg"
 
 NEW_MEM_HANDLER = MessageHandler(Filters.status_update.new_chat_members, new_member)
 LEFT_MEM_HANDLER = MessageHandler(Filters.status_update.left_chat_member, left_member)
